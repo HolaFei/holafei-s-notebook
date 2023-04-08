061ea2b888c9af37b2b23e12e985a92bbef8ef68
holafei <theside@qq.com>
2023-04-08 14:57:28 +0800

    加入对emotion和material-ui theme的支持

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,3 @@
-import { useState } from 'react'
-
-function App() {
-  return (
-    <div className="holafei-s-notebook-app">
-      Hello World
-    </div>
-  )
+export default function App() {
+  return <div>Hello World</div>
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import App from './App'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
-    </React.Fragment>
+    </ThemeProvider>
   </React.StrictMode>
 )
