e6cfa57ca59bb1346aa00b8bf023dbcbfbe2c186
hongfei <0049003335@znv.com>
2023-04-11 09:44:27 +0800

    update for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/holafei-s-notebook/',
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
